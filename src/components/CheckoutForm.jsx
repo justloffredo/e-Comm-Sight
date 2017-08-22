e0445b647f821e5262c13fe316b39d16a40fa5e2
Justin Loffredo <justloffredo@gmail.com>
2017-08-21 21:37:25 -0400

    Updated for for State dropdown

--- src/components/CheckoutForm.jsx
+++ src/components/CheckoutForm.jsx
@@ -23,83 +23,117 @@ class CheckoutForm extends Component {
 	_handleSubmit = (ev) => {
 		ev.preventDefault();
 
-		const firstName = this.state.firstName;
-		const lastName = this.state.lastName;
-		const addressStreet = this.state.addressStreet;
-		const city = this.state.city;
-		const userState = this.state.state;
-		const zipCode = this.state.zipCode;
-		const error = (firstName === "" || lastName === "" || addressStreet === "" || city === "" || userState === "" || zipCode === "") &&  "One or more of your fields is blank";
+		const { Name, addressStreet, city, zipCode } = this.state;
+		const error = (Name === "" || addressStreet === "" || city === "" || zipCode === "") &&  "One or more of your fields is blank";
 
 
-			(!firstName || !lastName || !addressStreet || !city || !userState || !zipCode) || this.props.onSubmit({
+		(!Name || !addressStreet || !city || !zipCode) || this.props.onSubmit({
 
 			firstName: this.state.firstName,
 			lastName: this.state.lastName,
 			addressStreet: this.state.addressStreet,
-			city: this.state.city,
 			userState: this.state.state,
 			zipCode: this.state.zipCode,
 		});
 
-			this.setState({ firstName: "", lastName: "", addressStreet: "", city: "", userState: "", zipCode: "", error });
-		};
+		this.setState({ firstName: "", lastName: "", addressStreet: "", city: "", userState: "", zipCode: "", error });
+	};
 
 	render() {
-		const { firstName } = this.state;
-		const { lastName } = this.state;
-		const { addressStreet } = this.state;
-		const { city } = this.state;
-		const { userState } = this.state;
-		const { zipCode } = this.state;
-		const { error }  = this.state;
+		const { Name, addressStreet, zipCode, error  } = this.state;
+		const options = [
+			{ key:'AL', text:'Alabama', value:'AL' },
+			{ key:'AK', text:'Alaska', value:'AK' },
+			{ key:'AZ', text:'Arizona', value:'AZ' },
+			{ key:'AR', text:'Arkansas', value:'AR' },
+			{ key:'CA', text:'California', value:'CA' },
+			{ key:'CO', text:'Colorado', value:'CO' },
+			{ key:'CT', text:'Connecticut', value:'CT' },
+			{ key:'DE', text:'Delaware', value:'DE' },
+			{ key:'DC', text:'Washington D.C.', value:'DC' },
+			{ key:'FL', text:'Florida', value:'FL' },
+			{ key:'GA', text:'Georgia', value:'GA' },
+			{ key:'HI', text:'Hawaii', value:'HI' },
+			{ key:'ID', text:'Idaho', value:'ID' },
+			{ key:'IL', text:'Illinois', value:'IL' },
+			{ key:'IN', text:'Indiana', value:'IN' },
+			{ key:'IA', text:'Iowa', value:'IA' },
+			{ key:'KS', text:'Kansas', value:'KS' },
+			{ key:'KY', text:'Kentucky', value:'KY' },
+			{ key:'LA', text:'Louisiana', value:'LA' },
+			{ key:'ME', text:'Maine', value:'ME' },
+			{ key:'MD', text:'Maryland', value:'MD' },
+			{ key:'MA', text:'Massachusetts', value:'MA' },
+			{ key:'MI', text:'Michigan', value:'MI' },
+			{ key:'MN', text:'Minnesota', value:'MN' },
+			{ key:'MS', text:'Mississippi', value:'MS' },
+			{ key:'MO', text:'Missouri', value:'MO' },
+			{ key:'MT', text:'Montana', value:'MT' },
+			{ key:'NE', text:'Nebraska', value:'NE' },
+			{ key:'NV', text:'Nevada', value:'NV' },
+			{ key:'NH', text:'New Hampshire', value:'NH' },
+			{ key:'NJ', text:'New Jersey', value:'NJ' },
+			{ key:'NM', text:'New Mexico', value:'NM' },
+			{ key:'NY', text:'New York', value:'NY' },
+			{ key:'NC', text:'North Carolina', value:'NC' },
+			{ key:'ND', text:'North Dakota', value:'ND' },
+			{ key:'OH', text:'Ohio', value:'OH' },
+			{ key:'OK', text:'Oklahoma', value:'OK' },
+			{ key:'OR', text:'Oregon', value:'OR' },
+			{ key:'PA', text:'Pennsylvania', value:'PA' },
+			{ key:'PR', text:'Puerto Rico', value:'PR' },
+			{ key:'RI', text:'Rhode Island', value:'RI' },
+			{ key:'SC', text:'South Carolina', value:'SC' },
+			{ key:'SD', text:'South Dakota', value:'SD' },
+			{ key:'TN', text:'Tennessee', value:'TN' },
+			{ key:'TX', text:'Texas', value:'TX' },
+			{ key:'UT', text:'Utah', value:'UT' },
+			{ key:'VT', text:'Vermont', value:'VT' },
+			{ key:'VA', text:'Virginia', value:'VA' },
+			{ key:'WA', text:'Washington', value:'WA' },
+			{ key:'WV', text:'West Virginia', value:'WV' },
+			{ key:'WI', text:'Wisconson', value:'WI' },
+			{ key:'WY', text:'Wyoming', value:'WY' },
+		];
 
 		return (
 			<Segment inverted>
 		    <Form inverted onSubmit={this._handleSubmit}>
 		      <Form.Group widths='equal'>
 		        <Form.Input
-							name = "firstName"
-							label='First name'
+							name = "Name"
+							label='Full Name'
 							type= "text"
-							placeholder='First name'
+							placeholder='Full Name'
 							onChange={this._handleChange}
 							 />
-		        <Form.Input
-							name = "lastName"
-							label='Last name'
-							placeholder='Last name'
-							type= "text"
-							onChange={this._handleChange}
-							/>
 						<Form.Input
 							name = "addressStreet"
 							label='Street Address'
 							placeholder='Street Address'
 							type= "text"
 							onChange={this._handleChange}
-							/>
+						/>
 						<Form.Input
 							name = "city"
 							label='City'
 							placeholder='City'
 							type= "text"
 							onChange={this._handleChange}
-							/>
-						<Form.Input
+						/>
+						<Form.Select
 							name= "state"
 							label='State'
 							placeholder='State'
-							type= "text"
-							onChange={this._handleChange}
-							/>
+							options = {options}
+						/>
 						<Form.Input
 							name= "zipCode"
 							label='Zip Code'
 							placeholder='Zip Code'
 							type= "text"
 							onChange={this._handleChange}
-							/>
+						/>
 		      </Form.Group>
 					{error && <div className = "error">{error}</div>}
 		      <Button type='submit'>Submit</Button>
